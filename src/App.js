f8f75f914e9b035065242777c953b1f7e2559a3d
unknown <alisabooraa@gmail.com>
2021-08-28 21:10:44 +0430

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ContactPage from "./pages/contact/ContactPage";
 import PhotoPage from './pages/photopage/PhotoPage';
 import Navbar2 from './pages/2-Navbar/Navbar2';
 import Contribution from './pages/contribution/Contribution'
-import Footer from './components/footer/Footer';
+
 
 
 
